42f25f8455127066aa711db2851a896d84901d19
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-13 19:09:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,13 @@
 def line(array)
   
+  # If array is empty...
   if array == []
     "The line is currently empty."
+  
+  # Return string listing ppl in line
   else
+    
+    
     "The line is currently:"
   
   
